0d986be7f95a925a9629ebaf73df64f34b228e2b
AHMEDJHOR <ahmedjhuar@gmail.com>
2026-08-13 08:23:03 -0700

    fix: resolve phpstan hasfactory errors

--- Backend/app/Models/Order.php
+++ Backend/app/Models/Order.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,8 +9,6 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'customer_id',
         'restaurant_id',
--- Backend/app/Models/Payment.php
+++ Backend/app/Models/Payment.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'order_id',
         'payment_method',
