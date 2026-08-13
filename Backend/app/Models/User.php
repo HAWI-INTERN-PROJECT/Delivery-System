32553ac3934672d8873016aa75898693bf07f09e
AHMEDJHOR <ahmedjhuar@gmail.com>
2026-08-13 15:22:06 -0700

    fix: correctly place HasFactory generic type annotation

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -12,12 +12,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-/**
- * @use HasFactory<UserFactory>
- */
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
+
+    /** @use HasFactory<UserFactory> */
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
